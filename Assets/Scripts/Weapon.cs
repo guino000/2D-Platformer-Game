edc55466d3525c01735caa15d38e4c4d912a91a0
Guilherme Canalli <guilherme.canalli5@gmail.com>
2019-08-26 21:45:25 +0200

    Implemented fire animations and player death and respawn on fall.

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,6 +8,11 @@ public class Weapon : MonoBehaviour
     public float Damage = 10;
     public LayerMask whatToHit;
 
+    public Transform BulletTrailPrefab;
+    public Transform MuzzleFlashPrefab;
+
+    public float effectSpawnRate = 10;
+    private float timeToSpawnEffect = 0;
     private float timeToFire = 0;
     Transform firePoint;
 
@@ -45,11 +50,28 @@ public class Weapon : MonoBehaviour
         Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
         Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
         RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
+
+        if (Time.time >= timeToSpawnEffect)
+        {
+            Effect();
+            timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+        }
+
         Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.cyan);
         if(hit.collider != null)
         {
             Debug.DrawLine(firePointPosition, hit.point, Color.red);
             Debug.Log("We hit " + hit.collider.name + " and did " + Damage + " damage!.");
         }
+    }
+
+    void Effect()
+    {
+        Instantiate(BulletTrailPrefab, firePoint.position, firePoint.rotation);
+        Transform clone = (Transform) Instantiate(MuzzleFlashPrefab, firePoint.position, firePoint.rotation);
+        clone.parent = firePoint;
+        float size = Random.Range(0.6f, 0.9f);
+        clone.localScale = new Vector3(size, size, size);
+        Destroy(clone.gameObject, 0.02f);
     }
 }
