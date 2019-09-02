2b6927ad87b220356b9022b3139ea68549f1d9a6
Guilherme Canalli <guilherme.canalli5@gmail.com>
2019-09-02 21:42:17 +0200

    Added hit effect to guns and camera shake while shooting

---
+++ Assets/CameraShake.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    public Camera mainCam;
+
+    private float shakeAmount = 0;
+
+    private void Awake()
+    {
+        if (mainCam == null)
+            mainCam = Camera.main;
+    }
+
+    public void Shake(float amt, float lenght)
+    {
+        shakeAmount = amt;
+        InvokeRepeating("DoShake", 0, 0.01f);
+        Invoke("StopShake", lenght);
+    }
+
+    void DoShake()
+    {
+        if(shakeAmount > 0)
+        {
+            Vector3 camPos = mainCam.transform.position;
+
+            float shakeAmtX = Random.value * shakeAmount * 2 - shakeAmount;
+            float shakeAmtY = Random.value * shakeAmount * 2 - shakeAmount;
+            camPos.x += shakeAmtX;
+            camPos.y += shakeAmtY;
+
+            mainCam.transform.position = camPos;
+        }
+    }
+
+    void StopShake()
+    {
+        CancelInvoke("DoShake");
+        mainCam.transform.localPosition = Vector3.zero;
+    }
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -10,11 +10,17 @@ public class Weapon : MonoBehaviour
 
     public Transform BulletTrailPrefab;
     public Transform MuzzleFlashPrefab;
+    public Transform HitPrefab;
 
     public float effectSpawnRate = 10;
     private float timeToSpawnEffect = 0;
     private float timeToFire = 0;
     Transform firePoint;
+
+    //Handle camera shaking
+    public float camShakeAmt = 0.1f;
+    public float camShakeLenght = 0.1f;
+    CameraShake cameraShake;
 
     private void Awake()
     {
@@ -22,6 +28,15 @@ public class Weapon : MonoBehaviour
         if(firePoint == null)
         {
             Debug.LogError("There is no firePoint gameobject assigned to weapon!");
+        }
+    }
+
+    private void Start()
+    {
+        cameraShake = GameMaster.gm.GetComponent<CameraShake>();
+        if(cameraShake == null)
+        {
+            Debug.LogError("No CameraShake script found on GM object.");
         }
     }
 
@@ -51,27 +66,60 @@ public class Weapon : MonoBehaviour
         Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
         RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
 
-        if (Time.time >= timeToSpawnEffect)
-        {
-            Effect();
-            timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
-        }
-
         Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition) * 100, Color.cyan);
         if(hit.collider != null)
         {
             Debug.DrawLine(firePointPosition, hit.point, Color.red);
             Debug.Log("We hit " + hit.collider.name + " and did " + Damage + " damage!.");
         }
+
+        if (Time.time >= timeToSpawnEffect)
+        {
+            Vector3 hitPosition;
+            Vector3 hitNormal;
+
+            if (hit.collider == null)
+            {
+                hitPosition = (mousePosition - firePointPosition) * 30;
+                hitNormal = new Vector3(9999, 9999, 9999);
+            }
+            else
+            {
+                hitPosition = hit.point;
+                hitNormal = hit.normal;
+            }
+
+            Effect(hitPosition, hitNormal);
+            timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+        }
     }
 
-    void Effect()
+    void Effect(Vector3 hitPosition, Vector3 hitNormal)
     {
-        Instantiate(BulletTrailPrefab, firePoint.position, firePoint.rotation);
+        Transform trail = (Transform) Instantiate(BulletTrailPrefab, firePoint.position, firePoint.rotation);
+        LineRenderer lr = trail.GetComponent<LineRenderer>();
+
+        if (lr != null)
+        {
+            lr.SetPosition(0, firePoint.position);
+            lr.SetPosition(1, hitPosition);
+        }
+
+        Destroy(trail.gameObject, 0.04f);
+
+        if(hitNormal != new Vector3(9999, 9999, 9999))
+        {
+            Transform hitParticle = (Transform) Instantiate(HitPrefab, hitPosition, Quaternion.FromToRotation(Vector3.right, hitNormal));
+            Destroy(hitParticle.gameObject, 1f);
+        }
+
         Transform clone = (Transform) Instantiate(MuzzleFlashPrefab, firePoint.position, firePoint.rotation);
         clone.parent = firePoint;
         float size = Random.Range(0.6f, 0.9f);
         clone.localScale = new Vector3(size, size, size);
         Destroy(clone.gameObject, 0.02f);
+
+        //Shake the camera
+        cameraShake.Shake(camShakeAmt, camShakeLenght);
     }
 }
