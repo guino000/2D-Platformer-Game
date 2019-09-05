09c7a77b5bf9d55dbf737696a6a00848a2b4db56
Guilherme Canalli <guilherme.canalli5@gmail.com>
2019-09-05 21:41:52 +0200

    Started game UI

--- Assets/WaveSpawner.cs
+++ Assets/WaveSpawner.cs
@@ -23,6 +23,8 @@ public class WaveSpawner : MonoBehaviour
     public Wave[] waves;
     private int nextWave = 0;
 
+    public Transform[] spawnPoints;
+
     public float timeBetweenWaves = 5f;
     public float waveCountdown;
 
@@ -32,6 +34,11 @@ public class WaveSpawner : MonoBehaviour
 
     private void Start()
     {
+        if (spawnPoints.Length == 0)
+        {
+            Debug.LogError("No spawn points referenced!");
+        }
+
         waveCountdown = timeBetweenWaves;
     }
 
@@ -41,9 +48,7 @@ public class WaveSpawner : MonoBehaviour
         {
             if (!EnemyIsAlive())
             {
-                // Begin a new round
-                Debug.Log("Wave completed!");
-                return;
+                WaveCompleted();
             }
             else
             {
@@ -61,6 +66,24 @@ public class WaveSpawner : MonoBehaviour
         else
         {
             waveCountdown -= Time.deltaTime;
+        }
+    }
+
+    void WaveCompleted()
+    {
+        Debug.Log("Wave Completed!");
+
+        state = SpawnState.COUNTING;
+        waveCountdown = timeBetweenWaves;
+
+        if(nextWave + 1 > waves.Length + 1)
+        {
+            nextWave = 0;
+            Debug.Log("All waves completed!");
+        }
+        else
+        {
+            nextWave++;
         }
     }
 
@@ -97,6 +120,7 @@ public class WaveSpawner : MonoBehaviour
     void SpawnEnemy(Transform enemy)
     {
         Debug.Log("Spawning enemy: " + enemy.name);
-        Instantiate(enemy, transform.position, transform.rotation); 
+        Transform sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Instantiate(enemy, sp.position, sp.rotation); 
     }
 }
